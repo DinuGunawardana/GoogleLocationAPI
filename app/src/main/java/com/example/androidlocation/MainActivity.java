8d3eb8e0d4ea4ae268d072efd0b5f23449f998fb
DinuGunawardana <62100212+DinuGunawardana@users.noreply.github.com>
2024-05-06 13:42:33 +0530

    Current Location Class Update

--- app/src/main/java/com/example/androidlocation/MainActivity.java
+++ app/src/main/java/com/example/androidlocation/MainActivity.java
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -22,73 +25,23 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity {
 
-    private final int FINE_PERMISSION_CODE = 1;
-    private GoogleMap myMap;
-    Location currentLocation;
-    FusedLocationProviderClient fusedLocationProviderClient;
+    private Button currentLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        getLastLocation();
+        currentLocation = findViewById(R.id.currentLocation);
 
-//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-//        mapFragment.getMapAsync(MainActivity.this);
-
-    }
-
-    private void getLastLocation() {
-
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, FINE_PERMISSION_CODE);
-            return;
-        }
-        Task<Location> task = fusedLocationProviderClient.getLastLocation();
-        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+        currentLocation.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onSuccess(Location location) {
-                if (location != null) {
-                    currentLocation = location;
-
-                    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-                    mapFragment.getMapAsync(MainActivity.this);
-                }
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), CurrentLocation.class));
             }
         });
     }
 
-
-    @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
-
-        myMap = googleMap;
-
-//        LatLng colombo = new LatLng(6.927079, 79.861244);
-//        myMap.moveCamera(CameraUpdateFactory.newLatLng(colombo));
-//        MarkerOptions options = new MarkerOptions().position(colombo).title("Colombo");
-//        options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
-//        myMap.addMarker(options);
-
-        LatLng colombo = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-        myMap.addMarker(new MarkerOptions().position(colombo).title("My Location"));
-        myMap.moveCamera(CameraUpdateFactory.newLatLng(colombo));
-
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == FINE_PERMISSION_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                getLastLocation();
-            }else {
-                Toast.makeText(this, "Location permission is denied, please allow the permission", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
 }
